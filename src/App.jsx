8b47a2300893f5cfa53ad1d044e9a38f9ab8624e
Cordelia Snape <snapecordelia@gmail.com>
2023-10-30 14:23:32 +0000

    added API lat, long and name data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-// import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [location, setLocation] = useState({});
+  const [search, setSearch] = useState("");
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  async function getLocation(event) {
+    event.preventDefault();
+    const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
+    const res = await axios.get(API);
+    setLocation(res.data[0]);
+  }
 
   return (
     <>
-      <p>Hello</p>
+      <h1>City Locator</h1>
+      <form onSubmit={getLocation}>
+        <input onChange={handleChange} placeholder="location" />
+        <button>Explore!</button>
+      </form>
+      <h2>{location.display_name}</h2>
+      <p>latitude: {location.lat}</p>
+      <p>longitude: {location.lon}</p>
     </>
   );
 }
